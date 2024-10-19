d61f7e4469b305e491294ce2a8b525d473da73b4
RajithaKWG <rkrishan894@gmail.com>
2024-10-19 19:23:54 +0530

    Added Heading

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Simple Todo App Using Material UI</h1>
+    </div>
   )
 }
 
